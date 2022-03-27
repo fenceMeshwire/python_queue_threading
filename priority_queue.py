6b9510db0120b2c441c014b960e0e2c5293de0ca
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-03-27 10:43:13 +0200

    Create priority_queue.py

---
+++ priority_queue.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Python 3.9.5
+
+# 03_priority_queue.py
+
+# Purpose: priority queue - sorts the elements in an ascending order
+
+# Dependency:
+import queue
+
+data = queue.PriorityQueue()
+data.put(7) # Adding elements with .put()
+data.put(21)
+data.put(-5)
+data.put(4)
+while not data.empty():
+    print(data.get()) # Retreiving elements with .get()
