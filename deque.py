92bb92ca3c53747fd78940ee5bc4d7fb5c5dd8f8
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-03-27 10:49:36 +0200

    Create deque.py
    Purpose: deque - double-ended queue

---
+++ deque.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+# Python 3.7.1
+
+# deque.py
+
+# Purpose: deque - double-ended queue
+
+# Dependency:
+import collections
+
+data = collections.deque([4, 9, 17])
+print('New created:', data)
+data_copy = data.copy()
+print('Copy of data:', data_copy)
+
+print("Printing out all elements: ", end="\n")
+for d in data:
+    print(data, end = "\n ")
+
+# Single elements
+try:
+    print('Second element from the left:', data[1])
+except:
+    print("This index doesn't exist")
+
+try:
+    print("First element from the right:", data[-1])
+except:
+    print("This index doesn't exist")
+
+# Multiply
+data = data * 2
+
+# Add
+data = data + data_copy
+
+# Clear deque
+data.clear()
