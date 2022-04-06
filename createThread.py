d77e72bb01f21c2f3e097baa8f6265f185c6f8bc
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-04-06 19:16:15 +0200

    Create createThread.py
    Purpose: Create a thread in Python3

---
+++ createThread.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env/python3
+
+# Python 3.9.5
+
+# createThread.py
+
+# Purpose: Create a thread
+
+# Dependencies
+import time, threading
+import sys
+
+def basic_function():
+    print('Starting the thread...')
+    for counter in range(10):
+        print(counter, time.time())
+        time.sleep(1.5)
+    print('End of thread')
+
+# Main program
+print('Main program')
+thread = threading.Thread(target=basic_function)
+thread.start()
+print('End of main program')
